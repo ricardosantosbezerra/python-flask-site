0bf1610031c7fd6ed9460c3fa761f097fbdb4b3a
Ricardo Santos Bezerra <127365559+ricardosantosbezerra@users.noreply.github.com>
2023-03-09 15:06:11 -0300

    Add files via upload

---
+++ site_meu.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+@app.route("/usuarios/<nome>")
+def usuarios(nome):
+    return render_template("usuarios.html", nome=nome)
+
+if __name__ == "__main__":
+    app.run(debug=True)
